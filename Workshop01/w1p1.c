baa155bc5fdf424bcb39280216c37f1a2d799d39
Andy Chong <116693074+andychong777@users.noreply.github.com>
2024-01-15 15:58:46 -0500

    Add files via upload

---
+++ Workshop01/w1p1.c
@@ -0,0 +1,35 @@
+/*****************************************************************************
+Workshop - #1 (Part-1)
+Full Name :Andy Chong
+Student ID#:150558229
+Email :achong11@myseneca.ca
+Section :NAA
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************/
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+
+    printf("Dear teacher,\n\n");
+
+    printf("  I promise I will work hard from this day onward.\n");
+    printf("  I acknowledge that practice is extremely important,\n");
+    printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+
+    printf("Sincerely,\n\n");
+
+    printf("Andy Chong\n");
+    printf("Student ID# 150558229\n");
+
+    return 0;
+
+}
---
+++ Workshop01/w1p2.c
@@ -0,0 +1,40 @@
+/*****************************************************************************
+Workshop - #1 (Part-2)
+Full Name :Andy Chong
+Student ID#:150558229
+Email :achong11@myseneca.ca
+Section :NAA
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************/
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Workshop 1 Part-2\n");
+    printf("=================\n\n");
+
+    printf("Using tab specifiers...\n");
+    printf("\tTab-1\tTab-2\tTab-3\n");
+    printf("\t-----\t=====\t-----\n\n");
+
+
+    printf("\\ this is a back-slash character!\n");
+    printf("%% this is a percent sign character!\n");
+    printf("\" this is a double-quote character!\n\n");
+
+    printf("My favourite quotes are:\n");
+    printf("1.\t\"Skill is only developed by hours and hours of work.\"\n");
+    printf("\t\t\t\t\t\t-Usain Bolt\n\n");
+
+    printf("2.\t\"It's not about having time. It's about making time.\"\n");
+    printf("\t\t\t\t\t\t-unknown\n\n");
+
+    printf("3.\t\"All of us do not have equal talent. But, all of us\n");
+    printf("\thave an equal opportunity to develop our talents.\"\n");
+    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
+
+    return 0;
+}
